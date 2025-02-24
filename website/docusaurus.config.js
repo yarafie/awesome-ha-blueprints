1a032219756ac628f27db6818a20a6021d7a4a17
Yasir Ra <yarafie@gmail.com>
2025-02-24 19:30:25 +0300

    Work-In-Progress

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -66,7 +66,8 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/yarafie/awesome-ha-blueprints/edit/main/',
+          editUrl:
+            'https://github.com/yarafie/awesome-ha-blueprints/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
