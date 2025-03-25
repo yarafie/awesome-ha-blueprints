024e52db23fbbcdfe4ac05406262ef15e959ee31
Yasir Ra <yarafie@gmail.com>
2025-03-25 20:49:38 +0300

    Added Support for Shelly Integration and added Shelly SHBTN-2 Button 1

--- website/src/components/blueprints_docs/requirements/controllers/ShellyRequirement.tsx
+++ website/src/components/blueprints_docs/requirements/controllers/ShellyRequirement.tsx
@@ -11,8 +11,8 @@ function ShellyRequirement({ required, refers, children }: ShellyRequirementProp
     <CustomRequirement name='Shelly Integration' required={required}>
       <p>
         If you plan to integrate the {refers} with Shelly, you must have this
-        integration set up. The Shelly integration can be configured from the 
-        Home Assistant UI. Check the documentation for full details on the 
+        integration set up. The Shelly integration can be configured from the
+        Home Assistant UI. Check the documentation for full details on the
         required hardware and how to set up Shelly on your system.
       </p>
       <p>{children}</p>
