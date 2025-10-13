df8106494b61469a3996c582596dd5ac6df401af
Yasir Ra <yarafie@gmail.com>
2025-10-13 19:18:35 +0300

    Work-In-Progress

--- website/src/plugins/blueprint-downloader-plugin/blueprint-downloader-plugin.js
+++ website/src/plugins/blueprint-downloader-plugin/blueprint-downloader-plugin.js
@@ -14,7 +14,7 @@ export default function blueprintRoutesPlugin(context) {
       const { siteDir } = context
       const blueprintsDir = path.resolve(siteDir, '../blueprints')
 
-      const blueprintFiles = glob.sync('**/*.yaml', {
+      const blueprintFiles = globSync('**/*.yaml', {
         cwd: blueprintsDir,
         absolute: true,
       })
