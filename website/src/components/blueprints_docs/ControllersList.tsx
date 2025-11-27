9cbde2197c11f6a2d2a10f7a45e710f31afe9216
Yasir Ra <yarafie@gmail.com>
2025-11-27 19:19:50 +0300

    Update project with new blueprint library architecture

--- website/src/components/blueprints_docs/ControllersList.tsx
+++ website/src/components/blueprints_docs/ControllersList.tsx
@@ -253,9 +253,11 @@ const ControllersList: React.FC = () => {
       </div>
       <div style={listStyle}>
         {filteredControllers.map((controller) => {
-          const imagePath = `/awesome-ha-blueprints/img/awesome-ha-blueprints-logo.png`
+          // const imagePath = `/awesome-ha-blueprints/img/awesome-ha-blueprints-logo.png`
           // const imagePath = `/awesome-ha-blueprints/img/controllers/${controller.id}.png`
-          // const imagePath = controllerImages[controller.id] || '/awesome-ha-blueprints/img/awesome-ha-blueprints-logo.png'
+          const imagePath =
+            controllerImages[controller.id] ||
+            '/awesome-ha-blueprints/img/awesome-ha-blueprints-logo.png'
           // const imagePath =
           //  `/awesome-ha-blueprints/img/controllers/${controller.id}.png` ||
           //  '/awesome-ha-blueprints/img/awesome-ha-blueprints-logo.png'
