cb282128585d2d42f6e86e4e77fe67e0c0d8f2a4
Yasir Ra <yarafie@gmail.com>
2025-11-27 12:27:31 +0300

    Update project with new blueprint library architecture

--- website/src/utils/controllerimages.ts
+++ website/src/utils/controllerimages.ts
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - file is generated at build time
-import controllerImagesData from '@generated/controllerimages.json'
+import controllerImagesData from '@generated/controllerimages'
 
 export type ControllerImagesMap = Record<string, string>
 
